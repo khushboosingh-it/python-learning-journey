e9859e5c1add0943d5a60e77e1071bd493f43568
Khushboo Singh <singharushi8962@gmail.com>
2026-06-09 15:49:59 +0530

    Add files via upload

---
+++ list_methods_practice.py
@@ -0,0 +1,43 @@
+# todays topic:update(),remove(),add()
+# list = ['k','h','u','s']
+# # list[0] = "y" ---> update
+# # list.remove("l") ----> remove
+# list.pop()
+# list.append("k")
+# list.insert(3,"k")
+# list.clear()
+# print(list)
+
+# word = ['h','u','s','h','b','o','o']
+# # user = input("choose (pop),(insert),(append),clear(): ")
+# user = input("Enter the word: ")
+# word.insert(0,user)
+# word.remove('u')
+# word.pop(-1)
+# print("added")
+# print("removed")
+# print("pop!")
+# print(word)
+# user = input("Add the element: ")
+
+# cart = ["Milk", "Bread", "Eggs", "Butter"]
+# cart.insert(1,"Cheese")
+# cart.remove("Butter")
+# cart.pop()
+# print(cart)
+
+# tasks = ["Wake Up", "Study Python",
+#           "Exercise", "Watch TV"]
+# tasks.insert(1,"Breakfast")
+# tasks.remove("Watch TV")
+# tasks.remove("Exercise")
+# tasks.replace("Study Python","Study Python for 2 hours")
+# print(tasks)
+
+playlist = ["Shape of You", "Believer",
+             "Perfect", "Faded"]
+playlist.insert(1,"Senorita")
+playlist.remove("Faded")
+playlist.pop(0)
+playlist[2] = "Perfect Remix"
+print(playlist)
